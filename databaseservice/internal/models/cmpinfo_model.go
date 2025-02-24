4aa6e99a437ed35009a6a56862f3c135777abf53
bentafat wail <150479778+wailbentafat@users.noreply.github.com>
2025-02-24 13:05:17 +0100

    jhshj

--- databaseservice/internal/models/cmpinfo_model.go
+++ databaseservice/internal/models/cmpinfo_model.go
@@ -4,15 +4,9 @@ import "gorm.io/gorm"
 
 type CmpInfo struct {
 	gorm.Model
-
-	CompanyID *uint   `gorm:"uniqueIndex;default:null"`
-	Company   *CmpData `gorm:"foreignKey:CompanyID;constraint:OnDelete:SET NULL"`
-
-	WorkSpeciality    string `gorm:"not null"`
-	Address          string `gorm:"not null"`
-	Email            string `gorm:"not null"`
-	Phone            string `gorm:"not null"`
-	Description      string `gorm:"not null"`
-	GeneralInformation string `gorm:"not null"`
+	CarbonWhereToExpected string `gorm:"not null"`
+	CarbonWhereToOther string
+	CarbonWhereTo string `gorm:"not null"`
+	
 }
 
--- databaseservice/internal/models/complin_model.go
+++ databaseservice/internal/models/complin_model.go
@@ -7,9 +7,10 @@ import "gorm.io/gorm"
 type Complain struct {
 	gorm.Model
 
-	CompanyID *uint   `gorm:"index;default:null"`
-	Company   *CmpData `gorm:"foreignKey:CompanyID;constraint:OnDelete:SET NULL"`
+\
 
 	Type         string `gorm:"not null"`
 	ComplainDesc string `gorm:"not null"`
+	RouteDist    string `gorm:"not null"`
+	Price        int    `gorm:"not null"`
 }
